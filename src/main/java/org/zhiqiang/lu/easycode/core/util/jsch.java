e73ca969224eef15fd03891933126501f240713f
Q <281747606@qq.com>
2021-07-14 12:43:08 +0800

    添加jsch 工具

--- src/main/java/org/zhiqiang/lu/easycode/core/util/jsch.java
+++ src/main/java/org/zhiqiang/lu/easycode/core/util/jsch.java
@@ -2,6 +2,11 @@ package org.zhiqiang.lu.easycode.core.util;
 
 import java.io.InputStream;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelExec;
@@ -12,33 +17,72 @@ import com.jcraft.jsch.Session;
  * Created by riot94 on 1/6/2017.
  */
 
-public class linux {
+public class jsch {
 
   public static void main(String[] args) {
   }
 
-  public Session session(String host, String user, String password) throws Exception {
-    InetAddress.getByName(host).isReachable(500);
-    java.util.Properties config = new java.util.Properties();
-    config.put("StrictHostKeyChecking", "no");
-    JSch jsch = new JSch();
-    Session session = jsch.getSession(user, host, 22);
-    session.setPassword(password);
-    session.setConfig(config);
-    session.setTimeout(1000);
-    session.connect();
-    return session;
+  public static Map<String, List<Map<String, String>>> exec_table(String host, int port, String user, String password,
+      String... commands) throws Exception {
+    Map<String, List<Map<String, String>>> map = new HashMap<>();
+    final CountDownLatch latch = new CountDownLatch(commands.length);
+    for (String c : commands) {
+      new Thread(new Runnable() {
+        public void run() {
+          Map<String, Object> res = new HashMap<>();
+          res.put("status", 200);
+          res.put("message", "OK");
+          try {
+            map.put(c, exec_table(host, port, user, password, c));
+          } catch (Exception e) {
+            map.put(c, new ArrayList<Map<String, String>>());
+          }
+          latch.countDown();
+        }
+      }).start();
+    }
+    latch.await();
+    return map;
   }
 
-  public StringBuffer exec(Session session, String... commands) throws Exception {
+  public static List<Map<String, String>> exec_table(String host, int port, String user, String password,
+      String command) throws Exception {
+    List<Map<String, String>> list = new ArrayList<>();
+    List<String> results = new ArrayList<>();
+    for (String s : exec(null, host, port, user, password, command).toString().split("\n")) {
+      results.add(s);
+    }
+    String[] titles = results.get(0).toLowerCase().split("\\s{2,}");
+    for (int i = 1; i < results.size(); i++) {
+      String[] ns = results.get(i).toLowerCase().split("\\s{2,}");
+      Map<String, String> map = new HashMap<>();
+      for (int t = 0; t < titles.length; t++) {
+        map.put(titles[t], ns[t]);
+      }
+      list.add(map);
+    }
+    return list;
+  }
+
+  public static StringBuffer exec(javax.websocket.Session sock_session, String host, int port, String user,
+      String password, String... commands) throws Exception {
     StringBuffer sb = new StringBuffer();
     String command = "";
     for (String c : commands) {
       command += " && " + c;
     }
     command = command.substring(4);
+
+    InetAddress.getByName(host).isReachable(500);
     java.util.Properties config = new java.util.Properties();
     config.put("StrictHostKeyChecking", "no");
+    JSch jsch = new JSch();
+    Session session = jsch.getSession(user, host, port);
+    session.setPassword(password);
+    session.setConfig(config);
+    session.setTimeout(1000);
+    session.connect();
+
     Channel channel = session.openChannel("exec");
     ((ChannelExec) channel).setCommand(command);
     channel.setInputStream(null);
@@ -56,6 +100,9 @@ public class linux {
         }
         String str = new String(tmp, 0, i);
         sb.append(str);
+        if (sock_session != null) {
+          sock_session.getBasicRemote().sendText(str);
+        }
       }
       while (error.available() > 0) {
         int i = error.read(tmp_error, 0, 1024);
@@ -64,6 +111,9 @@ public class linux {
         }
         String str = new String(tmp, 0, i);
         sb.append(str);
+        if (sock_session != null) {
+          sock_session.getBasicRemote().sendText(str);
+        }
       }
       if (channel.isEOF()) {
         break;
